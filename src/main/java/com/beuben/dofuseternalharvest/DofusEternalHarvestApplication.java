11c0343ad881117a2d88a03b2397dd943226f653
BeuBen <beuscart.ben@gmail.com>
2022-08-29 18:28:24 +0200

    initial commit

---
+++ src/main/java/com/beuben/dofuseternalharvest/DofusEternalHarvestApplication.java
@@ -0,0 +1,13 @@
+package com.beuben.dofuseternalharvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DofusEternalHarvestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DofusEternalHarvestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beuben/dofuseternalharvest/DofusEternalHarvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beuben.dofuseternalharvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DofusEternalHarvestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
