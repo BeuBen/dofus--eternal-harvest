9d56d6d7ea2f19ad250878d4f45fd6b0ba7d680b
BeuBen <beuscart.ben@gmail.com>
2022-09-18 19:16:15 +0200

    feat(soulstone-reading): clear tmp folder after update

--- src/main/java/com/beuben/dofuseternalharvest/services/SoulstoneReadingService.java
+++ src/main/java/com/beuben/dofuseternalharvest/services/SoulstoneReadingService.java
@@ -1,7 +1,9 @@
 package com.beuben.dofuseternalharvest.services;
 
+import com.beuben.dofuseternalharvest.utils.Constants;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,6 +11,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Optional;
 
 @Service
 public class SoulstoneReadingService {
@@ -28,18 +32,23 @@ public class SoulstoneReadingService {
       System.out.println(tesseractException.getMessage());
     }
 
-    //TODO clean temp folder after
-
     //TODO parse text + update call on metamob API
 
     //TODO before update call, call monsters to setup difference
+
+    var tmpFolder = new File(Constants.TMP_PATH);
+    var listFiles = Optional.ofNullable(tmpFolder.listFiles());
+    listFiles.ifPresent(files ->
+        Arrays.stream(files)
+            .forEach(this::deleteTmpFileWrapped)
+    );
 
     return text;
   }
 
   public Tesseract setupTesseract() {
     var tesseract = new Tesseract();
-    tesseract.setDatapath("src/main/resources/tessdata");
+    tesseract.setDatapath(Constants.TESSDATA_PATH);
     tesseract.setLanguage("eng");
     tesseract.setPageSegMode(1);
     tesseract.setOcrEngineMode(1);
@@ -48,13 +57,14 @@ public class SoulstoneReadingService {
 
   public File convertMultipartToFile(MultipartFile multipartFile) throws IOException {
 
-    var tmpPath = Paths.get("src/main/resources/static/tmp");
+    var tmpPath = Paths.get(Constants.TMP_PATH);
     if (!Files.exists(tmpPath)) {
       Files.createDirectory(tmpPath);
     }
 
     var localPath = String.join("",
-        "src/main/resources/static/tmp/",
+        Constants.TMP_PATH,
+        "/",
         multipartFile.getOriginalFilename());
 
     var file = new File(localPath);
@@ -66,5 +76,13 @@ public class SoulstoneReadingService {
     }
 
     return file;
+  }
+
+  public void deleteTmpFileWrapped(File file) {
+    try {
+      FileUtils.delete(file);
+    } catch (IOException ioException) {
+      System.out.println(ioException.getMessage());
+    }
   }
 }
--- src/main/java/com/beuben/dofuseternalharvest/utils/Constants.java
+++ src/main/java/com/beuben/dofuseternalharvest/utils/Constants.java
@@ -1,6 +1,12 @@
 package com.beuben.dofuseternalharvest.utils;
 
 public class Constants {
+
+  //METAMOB
   public static final String HEADER_API_KEY = "HTTP-X-APIKEY";
   public static final String HEADER_USER_KEY = "HTTP-X-USERKEY";
+
+  //SOULSTONE READING
+  public static final String TMP_PATH = "src/main/resources/static/tmp";
+  public static final String TESSDATA_PATH = "src/main/resources/tessdata";
 }
